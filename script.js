31d942b82ce70bf56b572b0ac083ba5ff8bbbc64
Guilherme <guilhermeisk@hotmail.com>
2022-08-09 11:08:02 -0300

    Projeto 8

---
+++ script.js
@@ -0,0 +1,20 @@
+const api= 'https://digimon-api.vercel.app/api/digimon'
+
+window.onload=function(){
+  
+  
+  fetch(api)
+  .then(data => data.json())
+  .then(data => {
+    for(let i=0;i<data.length;i++){
+      let digimon=`
+      <div id="digimon">
+        <img src="${data[i].img}">
+        <p>${data[i].level}</p>
+        <p>${data[i].name}</p>
+      </div>
+      `
+      document.getElementById('digimons').innerHTML+=digimon
+    }
+  })
+}
